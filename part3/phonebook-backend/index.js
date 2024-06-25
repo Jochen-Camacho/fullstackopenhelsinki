c4a24af6c7bf1288fee05d4d1a33989d6138f5a2
Jochen Joaquim Camacho <jochen4go@gmail.com>
2024-06-25 01:12:32 -0400

    Connecting to Database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const Person = require("./models/person");
 
 const app = express();
 
@@ -16,75 +18,41 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-// Utility - Generate ID
-const generateId = () => {
-  //   const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return Math.floor(Math.random() * 10000000);
-};
-
 // Getting the Phonebook Info
-app.get("/info", (req, res) => {
+app.get("/info", async (req, res) => {
+  const count = await Person.countDocuments({});
+  console.log(count);
   res.send(
-    `<p>Phonebook has info for ${
-      persons.length
-    } people</p>  <p>${new Date()}</p>`
+    `<p>Phonebook has info for ${count} people</p>  <p>${new Date()}</p>`
   );
 });
 
 // Getting all the persons
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((data) => res.json(data));
 });
 
 // Getting a single person
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findById(id)
+    .then((person) => {
+      res.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 // Deleting a single person
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (!person) {
-    res.status(404).end();
-  }
-  persons = persons.filter((p) => p.id !== id);
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  const id = req.params.id;
+  Person.findByIdAndDelete(id)
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
 });
 
 // Posting a new person
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body.name || !body.number) {
@@ -93,23 +61,76 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  if (persons.find((p) => p.name === body.name)) {
-    return res.status(400).json({
-      error: "name already exists",
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  res.json(person);
+  person
+    .save()
+    .then((savedPerson) => res.json(savedPerson))
+    .catch((error) => next(error));
 });
 
-const PORT = 3001;
+app.put("/api/persons/:id", (req, res, next) => {
+  const newPerson = {
+    name: req.body.name,
+    number: req.body.number,
+  };
+  Person.findByIdAndUpdate(req.params.id, newPerson, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (req, resp) => {
+  resp.status(404).send({
+    error: "unknown endpoint",
+  });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
+
+// Utility - Generate ID
+// const generateId = () => {
+//   //   const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+//   return Math.floor(Math.random() * 10000000);
+// };
+
+// let persons = [
+//   {
+//     id: 1,
+//     name: "Arto Hellas",
+//     number: "040-123456",
+//   },
+//   {
+//     id: 2,
+//     name: "Ada Lovelace",
+//     number: "39-44-5323523",
+//   },
+//   {
+//     id: 3,
+//     name: "Dan Abramov",
+//     number: "12-43-234345",
+//   },
+//   {
+//     id: 4,
+//     name: "Mary Poppendieck",
+//     number: "39-23-6423122",
+//   },
+// ];
