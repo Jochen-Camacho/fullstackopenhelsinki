f4b57051a52a718e9aaf8effe4b4da402beaea66
Jochen Joaquim Camacho <jochen4go@gmail.com>
2024-06-30 01:39:43 -0400

    deploying post validation

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -95,6 +95,8 @@ const errorHandler = (error, req, res, next) => {
 
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -106,31 +108,3 @@ const PORT = process.env.PORT;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
 
-// Utility - Generate ID
-// const generateId = () => {
-//   //   const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-//   return Math.floor(Math.random() * 10000000);
-// };
-
-// let persons = [
-//   {
-//     id: 1,
-//     name: "Arto Hellas",
-//     number: "040-123456",
-//   },
-//   {
-//     id: 2,
-//     name: "Ada Lovelace",
-//     number: "39-44-5323523",
-//   },
-//   {
-//     id: 3,
-//     name: "Dan Abramov",
-//     number: "12-43-234345",
-//   },
-//   {
-//     id: 4,
-//     name: "Mary Poppendieck",
-//     number: "39-23-6423122",
-//   },
-// ];
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -14,8 +14,22 @@ mongoose
   .catch((error) => console.log("error connecting to MongoDB:", error.message));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number:{
+    type: String,
+    validate: {
+      validator: function(v){
+        return /\d{2,3}-\d+$/.test(v);
+
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  },
 });
 
 personSchema.set("toJSON", {
@@ -25,7 +39,5 @@ personSchema.set("toJSON", {
     delete returnedObject.__v;
   },
 });
-
-const Person = mongoose.model("Person", personSchema);
 
 module.exports = mongoose.model("Person", personSchema);
